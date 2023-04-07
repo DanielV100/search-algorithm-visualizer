0fbe9b820dd2afcc9e7d0ce475efc1f3c58953e6
Daniel <daniel.vollmer@bechtle.com>
2023-04-07 16:38:17 +0200

    cleanup + implemented typewritter effect

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,8 @@
 <script lang="ts">
-  import { sayHelloWorld } from "./modules/test.svelte";
-  import Test from "./modules/test.svelte";
-  
-  let name:string = "Test"; 
-  let age:number = 19; 
-  sayHelloWorld("Test"); 
-  const increaseAge = () => {
-    age += 1; 
-  }
+  import { typewriteHeading } from "./modules/heading.svelte";
+  import Heading from "./modules/heading.svelte";
+  window.onmouseover = typewriteHeading; 
 </script>
 
+<Heading/>
 
-<main>
-  <Test/>
-  <h3>Hello World!</h3>
-  <p>Name: {age}</p>
-  <button on:click={increaseAge}>Increase Age</button>
-</main>
