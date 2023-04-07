d5190625a2c67aa912df6abcab845048675fb117
Daniel <daniel.vollmer@bechtle.com>
2023-04-07 15:39:50 +0200

    css cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import { sayHelloWorld } from "./modules/test.svelte";
+  import Test from "./modules/test.svelte";
+  
   let name:string = "Test"; 
   let age:number = 19; 
-
+  sayHelloWorld("Test"); 
   const increaseAge = () => {
     age += 1; 
   }
@@ -9,6 +12,7 @@
 
 
 <main>
+  <Test/>
   <h3>Hello World!</h3>
   <p>Name: {age}</p>
   <button on:click={increaseAge}>Increase Age</button>
