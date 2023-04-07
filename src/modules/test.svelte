64f4db9ce282fa2a4a307fe29a49de6c6ea94b0a
Daniel <daniel.vollmer@bechtle.com>
2023-04-07 16:27:03 +0200

    craeted typewritter effect

--- src/modules/test.svelte
+++ src/modules/test.svelte
@@ -1,7 +1,30 @@
 <script lang="ts" context="module">
-    export function sayHelloWorld(message:string) {
-        alert("Hello World!" + message); 
+    const headingElement = document.getElementById("typewritterHeading") as HTMLParagraphElement; 
+    const headingAsString:string = "how sorting works."; 
+    let i:number = 0;
+    
+    export function typewriteHeading():void {
+        if(i < headingAsString.length) {
+            document.getElementById("typewritterHeading").innerHTML += headingAsString.charAt(i); 
+            i++; 
+            setTimeout(typewriteHeading, 40); 
+        }
     }
 </script>
-<p>See</p>
-<p></p>
+<div class="typewritterHeadingContainer">
+    <p class="display-3">See</p>
+    <p id="typewritterHeading" class="display-3"></p>
+</div>
+
+<style>
+    p {
+        margin: 20px;
+    }
+    .typewritterHeadingContainer {
+        display: flex;
+    }
+    #typewritterHeading {
+        font-weight: 900;
+        margin-left: -5px;
+    }
+</style>
