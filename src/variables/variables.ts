5dcef65feec61bde5d8743bf0fa374feeb3dd324
Daniel <daniel.vollmer@bechtle.com>
2023-04-14 22:10:19 +0200

    cleanup

---
+++ src/variables/variables.ts
@@ -0,0 +1,10 @@
+    //bars
+    const barColor:string =  "BlueViolet"; 
+    const barColorWhileSwapping:string = "red"; 
+    //scrollbar
+    const scrollbarMin:string = "0";
+    const scrollbarMax:string = "150";  
+    const scrollbarStep:string = "5.0"; 
+    const scrollbarValue:string = "20"; 
+
+    export { barColor, barColorWhileSwapping, scrollbarMax, scrollbarMin, scrollbarStep, scrollbarValue}; 
