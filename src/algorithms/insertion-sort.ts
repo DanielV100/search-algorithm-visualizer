20bbfe24e71c2c4a6cae9771ffc1fb6b2bbde8ca
Daniel <daniel.vollmer@bechtle.com>
2023-04-14 23:51:04 +0200

    implemented insertion sort

---
+++ src/algorithms/insertion-sort.ts
@@ -0,0 +1,23 @@
+import { barAsDivElements } from "../modules/bars.svelte";
+import { sortingSpeedScrollbar } from "../modules/speedscrollbar.svelte";
+import { barColor, barColorWhileSwapping } from "../variables/variables";
+import { endSorting } from "./algo-utils";
+
+export async function insertionSort() {
+    for(let i = 0; i < barAsDivElements.length; i++) {
+        let j:number = i; 
+        let m:string = barAsDivElements[i].style.height; 
+        while(j > 0 && parseInt(barAsDivElements[j-1].style.height) > parseInt(m)) { 
+            await new Promise(resolve => setTimeout(resolve, parseInt(sortingSpeedScrollbar.value)));
+            barAsDivElements[j].style.height = barAsDivElements[j-1].style.height; 
+            barAsDivElements[j].style.background = barColorWhileSwapping; 
+            if(j < barAsDivElements.length-1) {
+                barAsDivElements[j+1].style.background = barColor; 
+            }
+            j--; 
+        }
+        barAsDivElements[j].style.height = m; 
+        barAsDivElements[j+1].style.background = barColor; 
+    }
+    endSorting(); 
+}
--- src/modules/buttons.svelte
+++ src/modules/buttons.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     import { bubbleSort } from "../algorithms/bubble-sort";
+  import { insertionSort } from "../algorithms/insertion-sort";
 </script>
 
 <button class="btn btn-primary" id="create-bars">Create bars</button>
 <button class="btn btn-secondary" on:click={bubbleSort}>Bubble Sort</button>
-<button class="btn btn-secondary">Insertion Sort</button>
+<button class="btn btn-secondary" on:click={insertionSort}>Insertion Sort</button>
 <button class="btn btn-secondary">Selection Sort</button>
 
 <style>
